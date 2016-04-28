85a2cd378e3b0d1652dc9a32c025a4ff3f107157
ismaelrodrigues <rodrigues.ismael@gmail.com>
2016-04-28 11:29:59 -0300

    Add I dont know what

--- branding/branding/local_settings.py
+++ branding/branding/local_settings.py
@@ -11,6 +11,18 @@ ALLOWED_HOSTS = ['*']
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Database
+# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
